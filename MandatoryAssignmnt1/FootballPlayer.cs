ba18c1383fbe371b1bf15ea1332fd866e8e7bdbf
Jonas Emil Nielsen <jona0222@edu.easj.dk>
2022-10-03 16:40:52 +0200

    Add project files.

---
+++ MandatoryAssignmnt1/FootballPlayer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MandatoryAssignmnt1
+{
+    public class FootballPlayer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int ShirtNumber { get; set; }
+
+        public FootballPlayer()
+        {
+
+        }
+        public FootballPlayer(int id, string name, int age, int shirtNumber  )
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            ShirtNumber = shirtNumber;
+        }
+
+        public void ValidateName()
+        {
+            if (Name == null) throw new ArgumentNullException("Navn er null");
+            if (Name.Length < 2) throw new ArgumentOutOfRangeException("Navn skal indeholde mindst 2 tegn");
+        }
+
+        public void ValidateAge()
+        {
+            if (Age < 2) throw new ArgumentOutOfRangeException("Alder skal være mere end 1 år");
+        }
+
+        public void ValidateShirtNumber()
+        {
+            if (ShirtNumber < 1 || ShirtNumber > 99) throw new ArgumentOutOfRangeException("Spiller-nummeret skal være mellem 1 og 99");
+        }
+    }
+}
---
+++ MandatoryAssignmnt1Tests/FootballPlayerTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MandatoryAssignmnt1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MandatoryAssignmnt1.Tests
+{
+    [TestClass()]
+    public class FootballPlayerTests
+    {
+        FootballPlayer noName = new FootballPlayer(0, null,11,77);
+        FootballPlayer tooShortName = new FootballPlayer(1, "A", 5, 5);
+        FootballPlayer tooLowAge = new FootballPlayer(2, "Peter", 0,11);
+        FootballPlayer tooLowShirtNumber = new FootballPlayer(3,"James",11,0);
+        FootballPlayer tooHighShirtNumber = new FootballPlayer(4,"Frank", 12,1100);
+        FootballPlayer testPlayer = new FootballPlayer(5,"John", 18, 69);
+
+        [TestMethod()]
+        public void FootballPlayerTest()
+        {
+            FootballPlayer footballPlayer = new FootballPlayer();
+            footballPlayer.Id = 1;
+            footballPlayer.Name = "Jack";
+            footballPlayer.Age = 13;
+            footballPlayer.ShirtNumber = 20;
+        }
+
+        [TestMethod()]
+        public void ValidateNameTest()
+        {
+            testPlayer.ValidateName();
+            Assert.ThrowsException<ArgumentNullException>(() => noName.ValidateName());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => tooShortName.ValidateName());
+        }
+
+        [TestMethod()]
+        public void ValidateAgeTest()
+        {
+            testPlayer.ValidateAge();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => tooLowAge.ValidateAge());
+        }
+
+        [TestMethod()]
+        [DataRow(1)]
+        [DataRow(2)]
+        [DataRow(40)]
+        [DataRow(98)]
+        [DataRow(99)]
+        public void ValidateShirtNumberTest(int shirtNumber)
+        {
+            testPlayer.ShirtNumber = shirtNumber;
+            testPlayer.ValidateShirtNumber();
+        }
+
+        [TestMethod()]
+        public void ValidateShirtNumberExceptionTest()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => tooLowShirtNumber.ValidateShirtNumber());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => tooHighShirtNumber.ValidateShirtNumber());
+        }
+    }
+}
